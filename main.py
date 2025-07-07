5b8f304c799464be0025665ed04117095c0a1797
Daniele Mottura <dany.mottu@gmail.com>
2025-07-07 10:43:31 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def homepage():
+    return render_template("index.html.jinja")
+
+@app.route("/StayFocus")
+def StayFocus():
+    return render_template("Stayfocus.html.jinja")
+
+@app.route("/NewsPopup")
+def NewsPopup():
+    return render_template("News_PopUp.html.jinja")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="8000")
